45bb359a93c5e05f50bae469ccd85e3ea04350c8
we-will-cheat <69360370+we-will-cheat@users.noreply.github.com>
2020-08-23 18:54:41 +0530

    Update brain.js

--- pg2/static/brain.js
+++ pg2/static/brain.js
@@ -1,7 +1,11 @@
 //var socket = io("localhost:3000");
 var socket = io("https://we-will-cheat-xx.glitch.me/");
-socket.emit("odd")
 
+socket.emit("odd");
 socket.on("odd",(obj)=>{
     document.getElementById("content").innerHTML=obj.data;
 });
+
+setInterval(function() {
+  socket.emit("odd");
+}, 0.2*60*1000);
